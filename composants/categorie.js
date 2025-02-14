9230e3a172f98839a10446c73d8e927dd973fc08
Xavier Ricard <143432388+Caporal6@users.noreply.github.com>
2025-02-14 08:42:45 -0500

    composant installer on peux commencer

---
+++ composants/categorie.js
@@ -0,0 +1,13 @@
+import { Text, StyleSheet, View, FlatList, Button, Alert, Pressable } from 'react-native'
+import { useState } from 'react'
+
+const Categorie = () => {
+    return (
+        <View>
+            <Text>Catégorie</Text>
+            <Text>Ceci est un composant simple pour afficher une catégorie.</Text>
+        </View>
+    );
+};
+
+export default Categorie;
